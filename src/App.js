3bdb626012e53ca94456f7484c63d43b25d4c932
Yusasive <yusufabdullahi2910@gmail.com>
2023-03-14 18:44:01 +0100

    Project Added

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import { PaystackButton } from 'react-paystack';
+import './App.css';
+
+const App = () => {
+  const publicKey = "pk_test_97910a6c014683477af502a9233a59ed5915217a";
+  const amount = 37500000;
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+
+  const resetForm = () => {
+    setEmail('');
+    setName('');
+    setPhone('');
+  };
+
+  const componentProps = {
+    email,
+    amount,
+    metadata: {
+      name,
+      phone,
+    },
+    publicKey,
+    text: 'Buy Now',
+    onSuccess: ({ reference }) => {
+      alert(
+        `Your purchase was successful! Transaction reference: ${reference}`
+      );
+      resetForm();
+    },
+    onClose: () => alert("Wait! You need this oil, don't go!!!!"),
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="item">
+          <div className="overlay-effect"></div>
+          <img
+            className="item-image"
+            src="https://images.unsplash.com/photo-1588872657578-7efd1f1555ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="product"
+          />
+          <div className="item-details">
+            <p className="item-details__title">Dell Laptop</p>
+            <p className="item-details__amount">NGN {amount / 100}</p>
+          </div>
+        </div>
+        <div className="checkout">
+          <div className="checkout-form">
+            <div className="checkout-field">
+              <label>Full Name</label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="checkout-field">
+              <label>Email</label>
+              <input
+                type="text"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="checkout-field">
+              <label>Phone Number</label>
+              <input
+                type="text"
+                id="phone"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
+            <PaystackButton className="paystack-button" {...componentProps} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
